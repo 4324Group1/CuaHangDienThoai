03b7bb621277de506a26b8d5eb6f5febbae17763
Trần Trung Hiếu <tranhieukk@gmail.com>
2017-11-24 10:44:23 +0700

    first

---
+++ view/admin_view/content-header.php
@@ -0,0 +1,11 @@
+ <section class="content-header">
+      <h1>
+      	<?php echo $pageHeader ?>
+       
+        <small><?php echo $pagedescription ?> </small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
